a4fd68c399736fad35f9d228cac8997f6874e8ba
AliciaGuz <gl0214032023@unab.edu.sv>
2024-08-22 19:49:20 -0600

    Agregar archivos de proyecto.

---
+++ CapaConexion/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Net;
+using CapaConexion.Models;
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        List<customers> Customers = new List<customers>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cargarbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection =
+                new SqlConnection
+                ("Data Source=DESKTOP-EP7G28B\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            MessageBox.Show("Conexión iniciada");
+            connection.Open();
+
+       
+            //-----------------------------------
+
+            String selectFrom = "";
+            selectFrom = selectFrom + "SELECT[CompanyName]" + "\n";
+            selectFrom = selectFrom + ",[ContactName]" + "\n";
+            selectFrom = selectFrom + ",[ContactTitle]" + "\n";
+            selectFrom = selectFrom + ",[Address]" + "\n";
+            selectFrom = selectFrom + ",[City]" + "\n";
+            selectFrom = selectFrom + ",[Region]" + "\n";
+            selectFrom = selectFrom + ",[PostalCode]" + "\n";
+            selectFrom = selectFrom + ",[Country]" + "\n";
+            selectFrom = selectFrom + ",[Phone]" + "\n";
+            selectFrom = selectFrom + ",[Fax]"+ "\n";
+            selectFrom = selectFrom + "FROM[dbo].[Customers]";
+
+            //-----------------------------------
+
+
+            SqlCommand comando = new SqlCommand(selectFrom, connection);
+            SqlDataReader reader = comando.ExecuteReader();
+
+           
+           
+            while (reader.Read())
+            {
+
+                customers customers = new customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "": (String)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                Customers.Add(customers);
+            }
+            dataGrid.DataSource = Customers;
+
+
+            connection.Close();
+            MessageBox.Show("Conexión finalizada con éxito");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var filtro = Customers.FindAll(x => x.CompanyName.StartsWith(tbFiltro.Text));
+            dataGrid.DataSource = filtro;
+        }
+    }
+}
