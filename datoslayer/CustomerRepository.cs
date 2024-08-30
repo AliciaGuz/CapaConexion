e9f6fd2fcb30a19e03df4bf06e2adffba39184b9
AliciaGuz <gl0214032023@unab.edu.sv>
2024-08-30 15:16:21 -0600

    SQL injection, mejorando la seguridad.

--- datoslayer/CustomerRepository.cs
+++ datoslayer/CustomerRepository.cs
@@ -66,10 +66,11 @@ namespace Datoslayer
                 selectForID = selectForID + "      ,[Phone] " + "\n";
                 selectForID = selectForID + "      ,[Fax] " + "\n";
                 selectForID = selectForID + "  FROM [dbo].[Customers] " + "\n";
-                selectForID = selectForID + $"  Where CustomerID = '{id}'";
+                selectForID = selectForID + $"  Where CustomerID = @customerID";
 
                 using (SqlCommand comando = new SqlCommand(selectForID, conexion))
                 {
+                    comando.Parameters.AddWithValue("customerID", id);
                     var reader = comando.ExecuteReader();
                     customers customers = null;
 
